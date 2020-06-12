9ed1df00db7ea181afec2b0cc10c82d7b1b5299b
tanoscar111 <lethanhtan281098>
2020-06-12 19:19:52 +0700

    fix form js

--- DOM/js form/index.js
+++ DOM/js form/index.js
@@ -5,13 +5,14 @@ var createEmail = document.createElement("p");
 var creatTextNode = document.createTextNode("Email");
 createEmail.appendChild(creatTextNode);
 $("email1").appendChild(createEmail);
-
-
 var createIcon = document.createElement("i");
 createIcon.className = "fas fa-plus-circle";
 createEmail.appendChild(createIcon);
 $("email1").appendChild(createEmail);
 
+
+
+//chức năng xoá 
 function removeElement() {
     var removeEl = this.parentElement;
     removeEl.remove();
@@ -30,6 +31,17 @@ var createPhone = document.createElement("p");
 var createTextPhone = document.createTextNode("Phone");
 createPhone.appendChild(createTextPhone);
 $("form").appendChild(createPhone);
+
+
+
+
+
+
+
+
+
+
+
 // fixx  ;
 // function createEL(tag) {
 //     return document.createElement(tag);
