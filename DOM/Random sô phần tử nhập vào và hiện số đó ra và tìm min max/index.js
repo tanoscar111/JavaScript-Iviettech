6daba4b4834f44c827ecac14711e66e7f516738e
tanoscar111 <lethanhtan281098>
2020-06-15 13:44:46 +0700

    fix max min

--- DOM/Random sô phần tử nhập vào và hiện số đó ra và tìm min max/index.js
+++ DOM/Random sô phần tử nhập vào và hiện số đó ra và tìm min max/index.js
@@ -1,28 +1,31 @@
-function $(id) {
-    return document.getElementById(id);
-}
-var createEmail = document.createElement("p");
-var creatTextNode = document.createTextNode("Email");
-createEmail.appendChild(creatTextNode);
-$("form").appendChild(createEmail);
+ function $(id) {
+     return document.getElementById(id);
+ }
+
+ function random() {
+
+ }
+ $("btn").onclick = function() {
+     //  var el_input = document.getElementsByTagName("input");
+     //  for (let i = 0; i < el_input.length; i++) {
+     //      var value_input = el_input[i];
+     //  var getValue_input = value_input.value;
+     //  getValue_input = parseInt(value_input);
+     // for(j=0;j<getValue_input;j++){
+
+     // }
+     var getValue_input = document.getElementById("input").value;
+     var arraysave_random = [];
+     getValue_input = parseInt(getValue_input);
+     for (let i = 1; i <= getValue_input; i++) {
+         var a = Math.floor(Math.random() * 100);
+         arraysave_random.push(a);
+     }
+     //  for (let j = 0; j < arraysave_random.length; j++) {
+     //      alert("so thu" + (j + 1) + "=" + arraysave_random[j]);
+
+     //  }
+     alert(Math.max.apply(Math, arraysave_random));
 
 
-var createIcon = document.createElement("i");
-createIcon.className = "fas fa-plus-circle";
-createEmail.appendChild(createIcon);
-$("form").appendChild(createEmail);
-
-
-var clickadd = document.getElementsByClassName("fa-plus-circle");
-clickadd[0].onclick = function() {
-    var createInput = document.createElement('input');
-    createInput.className = "form-control col-md-6";
-    $("form").appendChild(createInput);
-    var createIcon1 = document.createElement("i");
-    createIcon1.className = "fas fa-minus-circle";
-    createInput.appendChild(createIcon1);    
-}
-var createPhone = document.createElement("p");
-var createTextPhone = document.createTextNode("Phone");
-createEmail.appendChild(createTextPhone);
-$("phone").appendChild(createEmail);
+ }
