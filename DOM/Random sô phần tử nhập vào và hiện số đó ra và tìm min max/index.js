700e3320dd3e8ca743f071f6869766e1db702f95
tanoscar111 <tanoscar2810@gmail.com>
2020-06-15 01:26:13 -0700

    ahihi

--- DOM/Random sô phần tử nhập vào và hiện số đó ra và tìm min max/index.js
+++ DOM/Random sô phần tử nhập vào và hiện số đó ra và tìm min max/index.js
@@ -2,8 +2,8 @@
      return document.getElementById(id);
  }
 
- function random() {
-
+ function createl(tag) {
+     return document.createElement(tag)
  }
  $("btn").onclick = function() {
      //  var el_input = document.getElementsByTagName("input");
@@ -21,11 +21,22 @@
          var a = Math.floor(Math.random() * 100);
          arraysave_random.push(a);
      }
-     //  for (let j = 0; j < arraysave_random.length; j++) {
-     //      alert("so thu" + (j + 1) + "=" + arraysave_random[j]);
-
-     //  }
-     alert(Math.max.apply(Math, arraysave_random));
+      for (let j = 0; j < arraysave_random.length; j++) {
+        //   alert("so thu" + (j + 1) + "=" + arraysave_random[j]);
+        var createEl=createl("p"); 
+        var createNodeText=document.createTextNode("so thu" + (j + 1) + "=" + arraysave_random[j]);
+        createEl.appendChild(createNodeText);
+        $("result").appendChild(createEl);
+      }
+     //max min
+     var createEl1=createl("p"); 
+     var createNodeText1=document.createTextNode( "max=" + Math.max.apply(Math, arraysave_random));
+     createEl1.appendChild(createNodeText1);
+     $("max").appendChild(createEl1);  
+     var createEl2=createl("p"); 
+     var createNodeText2=document.createTextNode("Min=" +Math.min.apply(Math, arraysave_random));
+     createEl2.appendChild(createNodeText2);
+     $("min").appendChild(createEl2);
 
 
  }
