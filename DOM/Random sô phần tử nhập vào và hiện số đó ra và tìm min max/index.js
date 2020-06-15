e6c2bdb49e529cde7af353bb66a61a19661f2a17
tanoscar111 <lethanhtan281098>
2020-06-15 09:50:03 +0700

    random

---
+++ DOM/Random sô phần tử nhập vào và hiện số đó ra và tìm min max/index.js
@@ -0,0 +1,28 @@
+function $(id) {
+    return document.getElementById(id);
+}
+var createEmail = document.createElement("p");
+var creatTextNode = document.createTextNode("Email");
+createEmail.appendChild(creatTextNode);
+$("form").appendChild(createEmail);
+
+
+var createIcon = document.createElement("i");
+createIcon.className = "fas fa-plus-circle";
+createEmail.appendChild(createIcon);
+$("form").appendChild(createEmail);
+
+
+var clickadd = document.getElementsByClassName("fa-plus-circle");
+clickadd[0].onclick = function() {
+    var createInput = document.createElement('input');
+    createInput.className = "form-control col-md-6";
+    $("form").appendChild(createInput);
+    var createIcon1 = document.createElement("i");
+    createIcon1.className = "fas fa-minus-circle";
+    createInput.appendChild(createIcon1);    
+}
+var createPhone = document.createElement("p");
+var createTextPhone = document.createTextNode("Phone");
+createEmail.appendChild(createTextPhone);
+$("phone").appendChild(createEmail);
