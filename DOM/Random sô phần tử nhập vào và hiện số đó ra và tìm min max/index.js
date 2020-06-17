63b2307fc393042df3b93b14f54e6f4509a4d67f
tanoscar111 <lethanhtan281098>
2020-06-17 19:11:19 +0700

    fix code click one

--- DOM/Random sô phần tử nhập vào và hiện số đó ra và tìm min max/index.js
+++ DOM/Random sô phần tử nhập vào và hiện số đó ra và tìm min max/index.js
@@ -14,6 +14,10 @@
      // for(j=0;j<getValue_input;j++){
 
      // }
+     var el = document.getElementsByClassName("chid_rd");
+     if (el.length > 0) {
+         el[0].remove();
+     }
      var getValue_input = document.getElementById("input").value;
      var arraysave_random = [];
      getValue_input = parseInt(getValue_input);
@@ -21,22 +25,29 @@
          var a = Math.floor(Math.random() * 100);
          arraysave_random.push(a);
      }
-      for (let j = 0; j < arraysave_random.length; j++) {
-        //   alert("so thu" + (j + 1) + "=" + arraysave_random[j]);
-        var createEl=createl("p"); 
-        var createNodeText=document.createTextNode("so thu" + (j + 1) + "=" + arraysave_random[j]);
-        createEl.appendChild(createNodeText);
-        $("result").appendChild(createEl);
-      }
-     //max min
-     var createEl1=createl("p"); 
-     var createNodeText1=document.createTextNode( "max=" + Math.max.apply(Math, arraysave_random));
-     createEl1.appendChild(createNodeText1);
-     $("max").appendChild(createEl1);  
-     var createEl2=createl("p"); 
-     var createNodeText2=document.createTextNode("Min=" +Math.min.apply(Math, arraysave_random));
-     createEl2.appendChild(createNodeText2);
-     $("min").appendChild(createEl2);
+     var createDiv = createl("div");
+     createDiv.className = "chid_rd";
 
+     for (let j = 0; j < arraysave_random.length; j++) {
+         //   alert("so thu" + (j + 1) + "=" + arraysave_random[j]);
+         var createEl = createl("p");
+         var createNodeText = document.createTextNode("so thu" + (j + 1) + "=" + arraysave_random[j]);
+         createEl.appendChild(createNodeText);
+
+         createDiv.appendChild(createEl);
+
+     }
+
+
+     //max min
+     var createEl1 = createl("p");
+     var createNodeText1 = document.createTextNode("max=" + Math.max.apply(Math, arraysave_random));
+     createEl1.appendChild(createNodeText1);
+     createDiv.appendChild(createEl1);
+     var createEl2 = createl("p");
+     var createNodeText2 = document.createTextNode("Min=" + Math.min.apply(Math, arraysave_random));
+     createEl2.appendChild(createNodeText2);
+     createDiv.appendChild(createEl2);
+     $("add").appendChild(createDiv);
 
  }
