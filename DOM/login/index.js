b83ec5ec1cd65fa40805cd72c06ed2135dcae77a
tanoscar111 <52403954+tanoscar111@users.noreply.github.com>
2020-07-10 14:52:29 +0700

    hihi

--- DOM/login/index.js
+++ DOM/login/index.js
@@ -16,4 +16,26 @@ createUserName.appendChild(nodeUserName);
 var createItem = createEL("i");
 createItem.className = "fas fa-plus-circle";
 createUserName.appendChild(createItem);
+createItem.setAttribute("id","icon");
 $("userName").appendChild(createUserName);
+ function  removeInput(){
+     var deleteEL=this.parentElement;
+     deleteEL.remove();
+ }
+function createElementinput(){
+    var createDiv = createEL("div");
+    createDiv.className = "createDiv";
+    var createInput = createEL("input");
+    createInput.className = "input col-md-6";
+    var createi = createEL("i");
+    createi.className = "fas fa-minus-circle col-md-6";
+    createi.onclick = removeInput;
+    createDiv.appendChild(createInput);
+    createDiv.appendChild(createi);
+    $("divuserName").appendChild(createDiv);
+   
+   
+
+}
+
+$("icon").onclick=createElementinput;
